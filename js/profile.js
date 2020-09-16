4442b9964659df6e7c22faaaa1783f22acacb572
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-09-16 10:18:51 -0500

    Added white background

--- js/profile.js
+++ js/profile.js
@@ -6,7 +6,15 @@ $(document).ready(function(){
     $('.name').click(function() {
         $('#pictureMichael,.icon').toggle(300)
 
-    }
-    )
 
+
+
+        })
 })
+
+$(document).ready(function() {
+    $('.test-popup-link').magnificPopup({
+        type: 'image'
+        // other options
+    });
+});
