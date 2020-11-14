bd65f359752727c7889936775f88f983f5987eff
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-14 16:01:27 -0600

    adding three.js funtionality

--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,36 @@
 
 "use strict";
 
-$(document).ready(function(){
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    $('.name').click(function() {
-        $('#pictureMichael,.icon').toggle(300)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
 
 
 
 
-        })
-})
+function fizzbuzz(num) {
+// var fizzbuzz = function (num) {
+    for(var i = 1; i <= num; i++) {
+        if (i % 15 === 0) {
+            console.log("fizzbuzz");
+        } else if (i % 3 === 0) {
+            console.log("fizz")
+        } else if (i % 5 === 0) {
+            console.log("buzz")
+        } else {
+            console.log(i);
+        }
+    }
 
-$(document).ready(function() {
-    $('.test-popup-link').magnificPopup({
-        type: 'image'
-        // other options
-    });
-});
+}
+fizzbuzz(100);
