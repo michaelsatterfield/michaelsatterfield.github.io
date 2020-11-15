dff41a5210a51ee91e24bc952fcf5cbdcfad9552
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-14 18:09:24 -0600

    added a spinning triangle animation

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,21 @@
+const geometry = new THREE.BufferGeometry();
+// create a simple square shape. We duplicate the top left and bottom right
+// vertices because each vertex needs to appear once per triangle.
+const vertices = new Float32Array( [
+    -2.0, -2.0,  2.0,
+    1.0, -1.0,  1.0,
+    1.0,  1.0,  1.0,
 
-"use strict";
+    1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0, -1.0,  1.0
+] );
+
+// itemSize = 3 because there are 3 values (components) per vertex
+geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 2) );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const mesh = new THREE.Mesh( geometry, material );
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,16 +23,32 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+//sphere
+// const geometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
+// const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const sphere = new THREE.Mesh( geometry, material,mesh );
+scene.add( sphere );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
 
+const animate = function () {
+    requestAnimationFrame( animate );
 
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
 
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
 
 function fizzbuzz(num) {
 // var fizzbuzz = function (num) {
